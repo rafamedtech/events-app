a824756be6f2d6a0aa33729ff1c229e0cb032667
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-22 10:56:21 -0700

    routing update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link :to="{ name: 'EventList' }">Home</router-link> |
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
   </div>
   <router-view />
